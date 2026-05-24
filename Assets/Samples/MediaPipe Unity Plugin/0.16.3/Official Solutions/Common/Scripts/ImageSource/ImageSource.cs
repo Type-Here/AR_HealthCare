d55cd64dd709125ab9eb0cd1ca555e09a0d52398
Type-Here <94553428+Type-Here@users.noreply.github.com>
2026-05-24 21:05:14 +0200

    fix: update frame rate calculation in ImageSource to use refreshRateRatio

--- Assets/Samples/MediaPipe Unity Plugin/0.16.3/Official Solutions/Common/Scripts/ImageSource/ImageSource.cs
+++ Assets/Samples/MediaPipe Unity Plugin/0.16.3/Official Solutions/Common/Scripts/ImageSource/ImageSource.cs
@@ -31,12 +31,16 @@ namespace Mediapipe.Unity
       {
         width = resolution.width;
         height = resolution.height;
-        frameRate = resolution.refreshRate;
+        frameRate = resolution.refreshRateRatio.value;
       }
 
       public Resolution ToResolution()
       {
-        return new Resolution() { width = width, height = height, refreshRate = (int)frameRate };
+        var r = new Resolution();
+        r.width = width;
+        r.height = height;
+        r.refreshRateRatio = new RefreshRate { numerator = (uint)frameRate, denominator = 1 };
+        return r;
       }
 
       public override string ToString()
